d6df2eba466857ec801bc5310c4b42c46953eb63
Phitth <ipop.cool@gmail.com>
2016-11-28 11:25:45 +0700

    Update '.'
    Update 28/11/2016 11:25

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          <h1>Hello, world</h1>
+          <h1>Hello, world.</h1>
         </p>
       </div>
     );
